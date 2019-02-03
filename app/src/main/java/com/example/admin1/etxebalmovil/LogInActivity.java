7874f10f6118f03de33891157c6e3d1af995587e
Ibon Alonso <ibonalofer@gmail.com>
2019-02-03 20:41:37 +0100

    IAF: Nada

--- app/src/main/java/com/example/admin1/etxebalmovil/LogInActivity.java
+++ app/src/main/java/com/example/admin1/etxebalmovil/LogInActivity.java
@@ -1,7 +1,11 @@
 package com.example.admin1.etxebalmovil;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class LogInActivity extends AppCompatActivity {
 
