cf37f0fcba499535661ec48568c2e35228ac104c
IbonClase <ibon.alonsofe@elorrieta-errekamari.com>
2019-02-08 08:23:58 +0100

    IAF: quitada la lista en en inicio de sesion

--- app/src/main/java/com/example/admin1/etxebalmovil/FragmentoInicioSesionActivity.java
+++ app/src/main/java/com/example/admin1/etxebalmovil/FragmentoInicioSesionActivity.java
@@ -5,20 +5,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.admin1.etxebalmovil.model.json.JSONController;
 
 import java.util.UUID;
 
-public class FragmentoInicioSesionActivity extends FragmentoActivity {
+public class FragmentoInicioSesionActivity extends AppCompatActivity {
 
     //TODO que sea main activity y loguee
+
+    private EditText mUsuarioEditText;
+    private EditText mPasswordEditText;
+    private Button mIniciarSesion;
 
     public static Intent newIntent(Context packageContect) {
         Intent intent = new Intent(packageContect, FragmentoInicioSesionActivity.class);
         return intent;
     }
 
-    @Override
     protected Fragment createFragment() {
         return new FragmentoInicioSesion();
     }
@@ -26,6 +37,36 @@ public class FragmentoInicioSesionActivity extends FragmentoActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.iniciar_sesion_fragment_layout);
+
+        mUsuarioEditText = findViewById(R.id.editTextPalabra);
+        mPasswordEditText = findViewById(R.id.editTextPassword);
+        mIniciarSesion = findViewById(R.id.btnLogIn);
+        mIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                byte errorCode = JSONController.logInUser(mUsuarioEditText.getText().toString().trim(), mPasswordEditText.getText().toString().trim());
+                switch (errorCode) {
+                    case JSONController.NO_ERROR: {
+                        Toast.makeText(FragmentoInicioSesionActivity.this, "Bienvenido " + mUsuarioEditText.getText().toString().trim(), Toast.LENGTH_LONG).show();
+                        JSONController.getData();
+                        Intent intent = FragmentoListarActivity.newIntent(FragmentoInicioSesionActivity.this);
+                        startActivity(intent);
+                        //TODO preguntar si quiere log out
+
+
+                    }
+                    break;
+                    case JSONController.INPUT_ERROR:
+                        Toast.makeText(FragmentoInicioSesionActivity.this, getString(R.string.errorUsuario), Toast.LENGTH_LONG).show();
+                        break;
+                    case JSONController.OTHER_ERROR:
+                        Toast.makeText(FragmentoInicioSesionActivity.this, getString(R.string.errorOtro), Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        });
+
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
     }
